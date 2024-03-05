302ded35f92b94aeca49f4686eeeb2531b9f3a26
Matheus Fernandes <matheusfernandescerqueira@gmail.com>
2024-03-05 16:41:29 -0300

    Criando pagina Carrinho

---
+++ proj06f/src/pages/Carrinho.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Navegacao from "../components/Navegacao";
+import ProdutosExemplo from "../datas/ProdutoExemplo";
+
+export default function Carrinho() {
+    return<>
+    
+    </>
+}
